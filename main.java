6566ee54d8d1910b0b93da9d780fcd63a6943490
neev-sheth-shelat <cheeztarian@gmail.com>
2026-09-22 09:48:21 -0700

    Add deposit confirmation message to user

--- main.java
+++ main.java
@@ -286,6 +286,7 @@ public class main{
                 double depositAmount = scanner.nextDouble();
                 userBank.deposit(depositAmount);
                 System.out.println("Your new balance is: " + userBank.getBalance());
+                System.out.println("______________________________________________________________");
                
             }
 
